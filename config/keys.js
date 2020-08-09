bf9c7e2dc16647b2b2fb62910f4d65f4809fae31
thebigdavec <daverbcollison@gmail.com>
2020-08-09 23:11:03 +0100

    Implement front end with React

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dave:B1a7erd0am@cluster0.cep6q.mongodb.net/db1?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //Bodyparser Middleware
 app.use(bodyParser.json());
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongo
+mongoose
+.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
