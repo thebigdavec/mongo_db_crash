207115588bb234f9569156721b0a12f978f98151
thebigdavec <daverbcollison@gmail.com>
2020-08-10 13:27:53 +0100

    Initial implementation of redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-
-  render() {
-  return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
-  );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App">
+					<AppNavbar />
+					<ShoppingList />
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
