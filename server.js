640f3f21d2e21992c689d2f63c7c8dfb22e3c2c4
thebigdavec <daverbcollison@gmail.com>
2020-08-13 17:42:14 +0100

    remove body-parser dependency

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const items = require('./routes/api/items');
 
 const app = express();
-
-//Bodyparser Middleware
-app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
